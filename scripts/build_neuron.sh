edf76bf41587952ffcbb9d327883a08da81ab382
bcumming <bcumming@cscs.ch>
2018-06-15 10:27:14 +0200

    further build script refinement and add Arbor build

--- scripts/build_neuron.sh
+++ scripts/build_neuron.sh
@@ -1,35 +1,41 @@
-cd $base_path/build
+neuron_repo_path=$build_path/neuron
 
 msg "NEURON: starting build"
 
-# get the code
-msg "NEURON: cloning"
-git clone https://github.com/neuronsimulator/nrn neuron
-cd neuron
+# only check out code if not already checked out
+if [ ! -d "$neuron_repo_path/.git" ]
+then
+    # TODO: currently just using master on the development repo, because
+    #       there are no release tags. Maybe download the source?
+    msg "NEURON: cloning"
+    git clone https://github.com/neuronsimulator/nrn $neuron_repo_path
+fi
 
-# TODO: currently just using master on the development repo, because
-#       there are no release tags. Maybe download the source?
+cd $neuron_repo_path
 
-msg "NEURON: configure"
-./build.sh
-config_options=--prefix="$install_path"
-config_options="$config_options --without-iv"
-config_options="$config_options --with-nrnpython"
-# TODO: mpi
-#config_options="$config_options --with-mpi"
-./configure $config_options
+# only run configure steps if Makefile has not previously been generated.
+if [ ! -f "$neuron_repo_path/Makefile" ]
+then
+    msg "NEURON: configure"
+
+    ./build.sh > $build_path/neuron_configure_log
+    config_options=--prefix="$install_path"
+    config_options="$config_options --without-iv"
+    config_options="$config_options --with-nrnpython"
+    # TODO: mpi... config_options="$config_options --with-mpi"
+
+    ./configure $config_options >> $build_path/neuron_configure_log
+fi
 
 msg "NEURON: build"
 make -j6
 
 msg "NEURON: install"
-make install
+make install > $build_path/neuron_install_log
 
 # install python stuff
 msg "NEURON: python setup and install"
-cd src/nrnpython
-python setup.py install --prefix=$install_path
+cd "$neuron_repo_path/src/nrnpython"
+python setup.py install --prefix=$install_path > $build_path/neuron_python_install_log
 
-export PYTHONPATH=$install_path/lib/python3.6/site-packages/
-
-cd $base
+cd $base_path
