819434ea05d3eb4e138dde9d4bfcb82683452add
bcumming <bcumming@cscs.ch>
2018-09-20 15:16:32 +0200

    wip

---
+++ systems/daint-gpu.sh
@@ -0,0 +1,22 @@
+# set up environment for building on the multicore part of daint
+
+[ "$PE_ENV" = "CRAY" ] && module swap PrgEnv-cray PrgEnv-gnu
+module load daint-gpu
+module load CMake
+
+# PyExtensions is needed for cython, mpi4py and others.
+# It loads cray-python/3.6.1.1 which points python at version 3.6.1.1
+module load PyExtensions/3.6.1.1-CrayGNU-17.08
+ns_python=$(which python3)
+
+# load after python tools because easybuild...
+module swap gcc/6.2.0
+
+ns_cc=$(which cc)
+ns_cxx=$(which CC)
+ns_with_mpi=ON
+
+ns_arb_with_gpu=ON
+ns_arb_arch=haswell
+
+export CRAYPE_LINK_TYPE=dynamic
--- systems/daint-mc.sh
+++ systems/daint-mc.sh
@@ -1,20 +1,21 @@
 # set up environment for building on the multicore part of daint
 
-if [ $PE_ENV = "CRAY" ]
-then
-    echo "loading GNU programming model"
-    module swap PrgEnv-cray PrgEnv-gnu
-fi
+[ "$PE_ENV" = "CRAY" ] && module swap PrgEnv-cray PrgEnv-gnu
 module load daint-mc
+module load CMake
 
-# PyExtensions is needed for cython.
+# PyExtensions is needed for cython, mpi4py and others.
 # It loads cray-python/3.6.1.1 which points python at version 3.6.1.1
 module load PyExtensions/3.6.1.1-CrayGNU-17.08
+ns_python=$(which python3)
 
 # load after python tools because easybuild...
 module swap gcc/7.2.0
 
-export CC=`which cc`; export CXX=`which CC`
-export CRAYPE_LINK_TYPE=dynamic
+ns_cc=$(which cc)
+ns_cxx=$(which CC)
+ns_with_mpi=ON
 
-export with_mpi=true
+ns_arb_arch=broadwell
+
+export CRAYPE_LINK_TYPE=dynamic
