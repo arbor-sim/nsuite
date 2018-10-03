8ab79d3f97e9ccc99d8335fb104e8d6675c10f06
bcumming <bcumming@cscs.ch>
2018-10-03 10:26:41 +0200

    fix size of ring benchmark ranges

--- benchmarks/ring/table_line.sh
+++ benchmarks/ring/table_line.sh
@@ -1,9 +1,12 @@
-fid=$1
+fid="$1"
+if [ ! -f "$fid" ]; then
+    echo !!!! ERROR: file $fid does not exist
+else
+    tts=`grep ^model-run $fid | awk '{print $2}'`
+    ncell=`grep ^cell $fid | awk '{print $3}'`
+    ncomp=`grep ^cell $fid | awk '{print $7}'`
+    comp_sec=`echo "$ncomp / 10" | bc -l`
+    comp_rate=`echo "$comp_sec / $tts" | bc -l`
 
-tts=`grep ^model-run $fid | awk '{print $2}'`
-ncell=`grep ^cell $fid | awk '{print $3}'`
-ncomp=`grep ^cell $fid | awk '{print $7}'`
-comp_sec=`echo "$ncomp / 10" | bc -l`
-comp_rate=`echo "$comp_sec / $tts" | bc -l`
-
-printf "%7d%12d%12.3f%12.1f\\n" $ncell $ncomp $tts $comp_rate
+    printf "%7d%12d%12.3f%12.1f\n" $ncell $ncomp $tts $comp_rate
+fi
