014c552faa0b15a5409e4bf352784e65d841ad9e
bcumming <bcumming@cscs.ch>
2019-02-07 17:29:19 +0100

    isolate all benchmarks, and refactor so multiple benchmarks can use the same model

---
+++ benchmarks/models/ring/config.sh
@@ -0,0 +1,20 @@
+config=$1
+base_path=$2
+input_path=$3
+output_path=$4
+env_path=$5
+
+config_json="`pwd`/$config.json"
+
+[ ! -f "$config_json" ] && err "unable to find configuration file \"$config_json\"" && exit 1;
+
+config_name="ring-$config"
+
+engine_path="$base_path/benchmarks/engines/busyring"
+generator="$engine_path/generate_inputs.py"
+
+python "$generator" --idir "$input_path" --odir "$output_path" --config "$config_json" --bdir "$engine_path" --edir "$env_path" --sdir "$base_path/scripts"
+
+chmod +x "$input_path/run_arb.sh"
+chmod +x "$input_path/run_nrn.sh"
+chmod +x "$input_path/run_corenrn.sh"
