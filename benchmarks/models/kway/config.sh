7eb44b5edc16f5df04f9cd9a9a8d3e487ed6b454
Ben Cumming <bcumming@cscs.ch>
2019-03-15 13:44:38 +0100

    Install arbor benchmark executable in prefix/install/bin (#35)
    * Install arbor benchmark executables in prefix/install/bin.
    * Make benchmark runners quit gracefully if required simulator isn't in PATH.
    * Clean up errant tabs in bash scripts.

--- benchmarks/models/kway/config.sh
+++ benchmarks/models/kway/config.sh
@@ -2,7 +2,7 @@ config=$1
 base_path=$2
 input_path=$3
 output_path=$4
-env_path=$5
+prefix=$5
 
 config_json="`pwd`/$config.json"
 
@@ -13,7 +13,7 @@ config_name="ring-$config"
 engine_path="$base_path/benchmarks/engines/busyring"
 generator="$engine_path/generate_inputs.py"
 
-python3 "$generator" --idir "$input_path" --odir "$output_path" --config "$config_json" --bdir "$engine_path" --edir "$env_path" --sdir "$base_path/scripts"
+python3 "$generator" --idir "$input_path" --odir "$output_path" --config "$config_json" --bdir "$engine_path" --prefix "$prefix" --sdir "$base_path/scripts"
 
 chmod +x "$input_path/run_arb.sh"
 chmod +x "$input_path/run_nrn.sh"
