56904e1373e7768d3c72e7beccb47d4dcb918a40
Ben Cumming <bcumming@cscs.ch>
2019-03-22 17:53:53 +0100

    Osx (#47)
    * Fix various non-POSIXisms for Mac OS X compatibility.
    * Add C++14 requirement in CMake scripts to keep clang happy.

--- scripts/build_neuron.sh
+++ scripts/build_neuron.sh
@@ -14,14 +14,14 @@ if [ ! -f "$nrn_checked_flag" ]; then
     # "versioned" tar balls on the Neuron web site are not actually versioned.
     if [ "${ns_nrn_git_repo}" != "" ]; then
         msg "NEURON: cloning from $ns_nrn_git_repo to $nrn_repo_path"
-        git clone "$ns_nrn_git_repo" "$nrn_repo_path" &>> "${out}"
+        git clone "$ns_nrn_git_repo" "$nrn_repo_path" >> "${out}" 2>&1
         [ $? != 0 ] && exit_on_error "see ${out}"
 
         # check out the branch
         if [ "$ns_nrn_branch" != "master" ]; then
             msg "NEURON: check out branch $ns_nrn_branch"
             cd "$nrn_repo_path"
-            git checkout "$ns_nrn_branch" &>> "$out"
+            git checkout "$ns_nrn_branch" >> "$out" 2>&1
             [ $? != 0 ] && exit_on_error "see ${out}"
         fi
     # Otherwise download a tar ball.
@@ -34,13 +34,13 @@ if [ ! -f "$nrn_checked_flag" ]; then
         cd "$ns_build_path"
 
         msg "NEURON: download tarball ${ns_nrn_tarball} from ${ns_nrn_url}"
-        wget "$ns_nrn_url" &>> ${out}
+        wget "$ns_nrn_url" >> ${out} 2>&1
         [ $? != 0 ] && exit_on_error "see ${out}"
 
         msg "NEURON: untar ${ns_nrn_tarball}"
-        tar -xzf "${ns_nrn_tarball}" &>> ${out}
+        tar -xzf "${ns_nrn_tarball}" >> ${out} 2>&1
         [ $? != 0 ] && exit_on_error "see ${out}"
-        mv "${ns_nrn_path}" "$nrn_repo_path"  &>> ${out}
+        mv "${ns_nrn_path}" "$nrn_repo_path"  >> ${out} 2>&1
         [ $? != 0 ] && exit_on_error "see ${out}"
     fi
 
@@ -50,14 +50,17 @@ fi
 cd $nrn_repo_path
 
 # fix neuron to use CoreNEURON-compatble output
-sed -i -e 's/GLOBAL minf/RANGE minf/g' -e 's/TABLE minf/:TABLE minf/g' src/nrnoc/hh.mod
+# don't use -i argument to sed, because it is a non-posix extension that is implemented differently
+# on Linux and BSD.
+sed -e 's|GLOBAL minf|RANGE minf|g' -e 's|TABLE minf|:TABLE minf|g' src/nrnoc/hh.mod > tmp
+mv tmp src/nrnoc/hh.mod
 
 # only run configure steps if Makefile has not previously been generated.
 if [ ! -f "$nrn_repo_path/Makefile" ]
 then
     msg "NEURON: build.sh"
 
-    ./build.sh &>> "${out}"
+    ./build.sh >> "${out}" 2>&1
     [ $? != 0 ] && exit_on_error "see ${out}"
 
     config_options="--prefix=${ns_install_path} --exec-prefix=${ns_install_path}"
@@ -67,23 +70,23 @@ then
 
     msg "NEURON: configure $config_options"
 
-    ./configure $config_options &>> ${out}
+    ./configure $config_options >> ${out} 2>&1
     [ $? != 0 ] && exit_on_error "see ${out}"
 fi
 
 msg "NEURON: make"
-make -j $ns_makej &>> ${out}
+make -j $ns_makej >> "${out}" 2>&1
 [ $? != 0 ] && exit_on_error "see ${out}"
 
 msg "NEURON: install"
-make install &>> ${out}
+make install >> "$out" 2>&1
 [ $? != 0 ] && exit_on_error "see ${out}"
 
 # install python stuff
 msg "NEURON: python setup and install"
 cd "$nrn_repo_path/src/nrnpython"
-#"$ns_python" setup.py install --prefix=$ns_install_path &>> ${out}
-python setup.py install --prefix=$ns_install_path &>> ${out}
+#"$ns_python" setup.py install --prefix=$ns_install_path >> ${out} 2>&1
+python3 setup.py install --prefix=$ns_install_path >> ${out} 2>&1
 [ $? != 0 ] && exit_on_error "see ${out}"
 
 cd $ns_base_path
