d072492ac5cf0ebd5a9aee3df6bbd0c56f5cab5e
akuesters <42005107+akuesters@users.noreply.github.com>
2019-07-19 11:15:01 +0200

    update jureca env to new 2019a stage for mc, gpu stays at stage 2018b (#71)
    1) mc version now at new software stage 2019a on Jureca
    2) gpu version stays at software stage 2018b (since CUDA10 with MVAPICH2 fails and CUDA9 not available at new stage)
    
    Fixes #51

--- systems/jureca-mc.sh
+++ systems/jureca-mc.sh
@@ -4,32 +4,36 @@
 ns_sysname="jureca-mc"
 
 # set up environment for building on the multicore part of jureca
+module purge
+module use /usr/local/software/jureca/OtherStages/
+module load Stages/2019a
 
-module load CMake/3.13.0
+module load GCC
+module load ParaStationMPI
+module load CMake
 
-module load Python/3.6.6
-ns_python=python3
-
-# load before mpi4py because required
-module load GCC/8.2.0 ParaStationMPI/5.2.1-1
+module load Python/3.6.8
+module load SciPy-Stack/2019a-Python-3.6.8
+ns_python=$(which python3)
 
 # for (core)neuron
-module load mpi4py/3.0.0-Python-3.6.6
+module load mpi4py/3.0.1-Python-3.6.8
 module load flex/2.6.4
-module load Bison/.3.1
+
+# for validation tests
+module load netCDF/4.6.3
 
 ### compilation options ###
-
 ns_cc=$(which mpicc)
 ns_cxx=$(which mpicxx)
 ns_with_mpi=ON
 
 ns_arb_arch=haswell
+ns_arb_branch=master
 
 ns_makej=20
 
 ### benchmark execution options ###
-
 ns_threads_per_core=2
 ns_cores_per_socket=12
 ns_sockets=2
@@ -37,6 +41,8 @@ ns_threads_per_socket=24
 
 # activate budget via jutil env activate -p <cproject> -A <budget> before running the benchmark
 run_with_mpi() {
-    echo ARB_NUM_THREADS=$ns_threads_per_socket srun -n $ns_sockets -c $ns_threads_per_socket "${@}" 
-    ARB_NUM_THREADS=$ns_threads_per_socket srun -n $ns_sockets -c $ns_threads_per_socket "${@}" 
+    export ARB_NUM_THREADS=$ns_threads_per_socket
+    export OMP_NUM_THREADS=$ns_threads_per_socket
+    echo srun -n$ns_sockets -N1 -c$ns_threads_per_socket "${@}"
+    srun -n$ns_sockets -N1 -c$ns_threads_per_socket "${@}"
 }
