43a1dbd405f132dc04195da9259c195b21449b10
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-03-21 14:39:12 +0100

    Do not validate. Ever.

--- systems/gh.sh
+++ systems/gh.sh
@@ -15,12 +15,13 @@ ns_arb_git_repo='https://github.com/arbor-sim/arbor.git'
 ns_arb_branch='master'
 
 ns_makej=4
-
+ns_validate=disable
 ### benchmark execution options ###
 ns_threads_per_core=2
 ns_cores_per_socket=12
 ns_sockets=1
 ns_threads_per_socket=2
+
 
 run_with_mpi() {
     export ARB_NUM_THREADS=$ns_threads_per_socket
